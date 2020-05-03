d635b61151ac0fd643cb08daba95d54bfb1dd51a
LombardoAndrea195 <l.andrea195@live.it>
2020-05-03 12:22:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/andrea/shareyoursport/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.andrea.shareyoursport;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDe
+
+    }
+}
