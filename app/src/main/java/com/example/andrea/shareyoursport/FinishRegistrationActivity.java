5df57fa09f8c0dd8b4dddf7e1b8460e18bb134f4
LombardoAndrea195 <l.andrea195@live.it>
2020-05-05 11:14:55 +0200

    version 05/05/20 11.14
    Signed-off-by: LombardoAndrea195 <l.andrea195@live.it>

--- app/src/main/java/com/example/andrea/shareyoursport/FinishRegistrationActivity.java
+++ app/src/main/java/com/example/andrea/shareyoursport/FinishRegistrationActivity.java
@@ -1,10 +1,17 @@
 package com.example.andrea.shareyoursport;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,9 +20,87 @@ public class FinishRegistrationActivity extends AppCompatActivity {
     private Button next;
     private CircleImageView picture;
     private Uri imageUri;
+
+    private static final int PICK_IMAGE = 100;
+     private TextView Name1;
+    private TextView Surname1;
+      private String Sex;
+    public java.util.Date validateDateFormat(String dateToValdate) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HHmmss");
+        //To make strict date format validation
+        formatter.setLenient(false);
+        java.util.Date parsedDate = null;
+        try {
+            parsedDate = formatter.parse(dateToValdate);
+            System.out.println("++validated DATE TIME ++"+formatter.format(parsedDate));
+
+        } catch (ParseException e) {
+            //Handle exception
+        }
+        return parsedDate;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_finish_registration);
+        Bundle extras = getIntent().getExtras();
+        Name1 = (TextView) findViewById(R.id.firstname);
+        Surname1 = (TextView) findViewById(R.id.secondname);
+        picture = (CircleImageView) findViewById(R.id.profile_image);
+
+        if (extras != null) {
+            String email = extras.getString("Email");
+            String Name = extras.getString("Name");
+            String Surname = extras.getString("Surname");
+            String password = extras.getString("password");
+            String valueUri = extras.getString("URI_photo");
+            String Date =extras.getString("Date");
+            String Sex =extras.getString("Sex");
+            String City=extras.getString("City");
+            Uri imageUri = Uri.parse(valueUri);
+            picture.setImageURI(imageUri);
+            Name1.setText(Name);
+            Surname1.setText(Surname);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+
+
+
+
+                Date birthday = validateDateFormat(Date.toString());
+
+
+
+
+                Intent intent = new Intent(FinishRegistrationActivity.this, MenuActivity.class);
+                intent.putExtra("Name", extras.getString("Name"));
+                intent.putExtra("Surname", extras.getString("Surname"));
+                intent.putExtra("Email", extras.getString("Email"));
+                intent.putExtra("City", extras.getString("City"));
+                intent.putExtra("Date", extras.getString("Date"));
+                intent.putExtra("URI_photo", imageUri.toString());
+                intent.putExtra("password", extras.getString("password"));
+                intent.putExtra( "Sex",Sex);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode==RESULT_OK && requestCode==PICK_IMAGE){
+            imageUri=data.getData();
+            picture.setImageURI(imageUri);
+
+        }
     }
 }
--- app/src/main/java/com/example/andrea/shareyoursport/SecondActivity.java
+++ app/src/main/java/com/example/andrea/shareyoursport/SecondActivity.java
@@ -1,14 +1,21 @@
 package com.example.andrea.shareyoursport;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -18,63 +25,143 @@ public class SecondActivity extends AppCompatActivity {
     private CircleImageView picture;
     private Uri imageUri;
     private EditText City;
+    private Button Bback;
     private static final int PICK_IMAGE = 100;
     private EditText Date;
     private TextView Name1;
     private TextView Surname1;
+    private Switch Male;
+    private Switch Female;
+    private boolean M=false,F=false;
+    private String Sex;
+    public Date validateDateFormat(String dateToValdate) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        //To make strict date format validation
+        formatter.setLenient(false);
+        java.util.Date parsedDate = null;
+        try {
+            parsedDate = formatter.parse(dateToValdate);
+            System.out.println("++validated DATE TIME ++"+formatter.format(parsedDate));
+
+        } catch (ParseException e) {
+            Date.setError("Inserirela data come indiata dd-MM-yyyy");
+            Date.requestFocus();
+        }
+        return parsedDate;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Bundle extras = getIntent().getExtras();
-        Name1=(TextView) findViewById(R.id.firstname);
-        Surname1=(TextView) findViewById(R.id.secondname);
-
+        Bback=(Button) findViewById(R.id.back);
+        Name1 = (TextView) findViewById(R.id.firstname);
+        Surname1 = (TextView) findViewById(R.id.secondname);
+        Female = (Switch) findViewById(R.id.F);
+        Male = (Switch) findViewById(R.id.M);
         picture = (CircleImageView) findViewById(R.id.profile_image);
+        if (extras != null) {
+            String email = extras.getString("Email");
+            String Name = extras.getString("Name");
+            String Surname = extras.getString("Surname");
+            String password = extras.getString("password");
+            String valueUri = extras.getString("URI_photo");
 
+            Uri imageUri = Uri.parse(valueUri);
+            picture.setImageURI(imageUri);
+            Name1.setText(Name);
+            Surname1.setText(Surname);
+        Bback.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+              Intent  intent = new Intent(SecondActivity.this , FormRegisterActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//Bundle is optional
+//end Bundle
+                startActivity(intent);
+            }
+        });
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (extras != null) {
-                    String email = extras.getString("Email");
-                    String Name = extras.getString("Name");
-                    String Surname = extras.getString("Surname");
-                    String password = extras.getString("password");
-                    String value5 = extras.getString("picture");
-                    Uri imageUri = Uri.parse(value5);
-                    picture.setImageURI(imageUri);
-                    Name1.setText(Name);
-                    Surname1.setText(Surname);
 
 
+
+
+                Male.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            M = true;
+                        } else {
+                            M = false;
+                        }
+                    }
+                });
+                Female.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if (isChecked) {
+                            F = true;
+                        } else {
+                            F = false;
+                        }
+                    }
+                });
+                if (F == true && M == true) {
+                    Female.setError("Set only one Sex option");
+                    Male.setError("Set only one Sex option");
+                    Female.requestFocus();
+                    Male.requestFocus();
+
+                    Toast.makeText(SecondActivity.this, "Select only one", Toast.LENGTH_LONG).show();
+                } else if (F == false && M == false) {
+                    Female.setError("Set one Sex option");
+                    Male.setError("Set one Sex option");
+                    Female.requestFocus();
+                    Male.requestFocus();
+
+                    Toast.makeText(SecondActivity.this, "Select one", Toast.LENGTH_LONG).show();
+
+                } else if (F == true && M == false) {
+                    Sex = "Female";
+                } else {
+                    Sex = "Male";
                 }
-                if(Date.getText().toString().isEmpty()){
+                if (Date.getText().toString().isEmpty()) {
                     Date.setError("Please enter your birthday's date:");
                     Date.requestFocus();
 
                     Toast.makeText(SecondActivity.this, "Field is empty", Toast.LENGTH_LONG).show();
 
                 }
-                if (City.getText().toString().isEmpty() ) {
+
+                Date birthday = validateDateFormat(Date.toString());
+
+                if (City.getText().toString().isEmpty()) {
                     City.setError("Please enter your city:");
                     City.requestFocus();
 
                     Toast.makeText(SecondActivity.this, "Field is empty", Toast.LENGTH_LONG).show();
                 }
-                else if (.isEmpty()){
-
-                    password.setError("Please enter your password:");
-                    password.requestFocus();
-
-                    Toast.makeText(LoginActivity.this, "Field password is empty", Toast.LENGTH_LONG).show();
 
 
+                Intent intent = new Intent(SecondActivity.this, FinishRegistrationActivity.class);
+                intent.putExtra("Name", Name1.getText().toString());
+                intent.putExtra("Surname", Surname1.getText().toString());
+                intent.putExtra("Email", extras.getString("Email"));
+                intent.putExtra("City", City.getText().toString());
+                intent.putExtra("Date", Date.getText().toString());
+                intent.putExtra("URI_photo", imageUri.toString());
+                intent.putExtra("password", extras.getString("password"));
+                intent.putExtra( "Sex",Sex);
+                startActivity(intent);
 
-                });
+            }
+        });
 
+    }}
 
-        }
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
@@ -84,3 +171,4 @@ public class SecondActivity extends AppCompatActivity {
 
         }
 }
+    }
