d4c90f2efc588422b76dcdc2a563172adb9f4617
LombardoAndrea195 <l.andrea195@live.it>
2020-05-05 19:14:00 +0200

    version 05/05/20 19.30
    Signed-off-by: LombardoAndrea195 <l.andrea195@live.it>

--- app/src/main/java/com/example/andrea/shareyoursport/MenuActivity.java
+++ app/src/main/java/com/example/andrea/shareyoursport/MenuActivity.java
@@ -1,11 +1,14 @@
 package com.example.andrea.shareyoursport;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
-
+    Button searchButton,newButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,23 @@ public class MenuActivity extends AppCompatActivity {
         * 3) login basato su Registrazione
         * 4) login basato su login normale tutte devono portare a questa activity
         * */
+        searchButton=(Button) findViewById(R.id.search_event);
 
-    }
+        newButton=(Button) findViewById(R.id.new_event);
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this, NewEventActivity.class));
+
+            }
+        });
+
+     searchButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MenuActivity.this, SearchEventActivity.class));
+
+        }
+    });
+}
 }
---
+++ app/src/main/java/com/example/andrea/shareyoursport/SearchEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.andrea.shareyoursport;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_event);
+    }
+}
